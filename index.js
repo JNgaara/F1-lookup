0af8b8dad7fa9e2ca4861e93c09b3eea1e582cf3
Joshua Ngaara <joshua.ngaara@student.moringaschool.com>
2024-12-19 00:25:36 +0200

    lk

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ fetch('https://jsonplaceholder.typicode.com/todos/1')
       .then(json => console.log(json))
     //   fetch code to fecth and connect to placehodler api
     //   in practise api on wednesday in class morning standup
+    function callAPI(){
+      fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json))
+    }

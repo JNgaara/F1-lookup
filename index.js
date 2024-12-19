eb64da136c1bb3849f3b90f495b34933e26d60ad
Joshua Ngaara <joshua.ngaara@student.moringaschool.com>
2024-12-19 09:09:14 +0200

    drivers  api link

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ fetch('https://jsonplaceholder.typicode.com/todos/1')
       .then(json => console.log(json))
     //   fetch code to fecth and connect to placehodler api
     //   in practise api on wednesday in class morning standup
-    function callCONSTRUCTORS(){
+    function callCONSTRUCTORSAPI(){
       fetch('http://ergast.com/api/f1/constructors')
+      .then(response => response.json())
+      .then(json => console.log(json))
+    }
+
+    function callDriversAPI(){
+      fetch('http://ergast.com/api/f1/drivers')
       .then(response => response.json())
       .then(json => console.log(json))
     }

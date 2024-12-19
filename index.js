6b04f59a935265cb350b8e8fa4ccadd913cf9168
Joshua Ngaara <joshua.ngaara@student.moringaschool.com>
2024-12-19 21:03:54 +0200

    quick change blue form 2

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
 alert("Welcome to F1 lookup");
-function callCONSTRUCTORSAPI(){
-      fetch('http://ergast.com/api/f1/constructors')
-      .then(response => response.json())
-      .then(json => console.log(json))
-    }
-    const searchObj = [ team: ' ', driver:' ' , season: ' '  ];
-    let userSearch = [];
-    const uForm = document.getElementById("formF1Input";)
-    const inputTeam = document.getElementById('team');
-    const inputDriver = document.getElementById('driver');
-    const inputSeason = document.getElementById('Season');
+// function callCONSTRUCTORSAPI(){
+//       fetch('http://ergast.com/api/f1/constructors')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+//     }
+//     const searchObj = [ team: ' ', driver:' ' , season: ' '  ];
+//     let userSearch = [];
+//     const uForm = document.getElementById("formF1Input";)
+//     const inputTeam = document.getElementById('team');
+//     const inputDriver = document.getElementById('driver');
+//     const inputSeason = document.getElementById('Season');
     
-    document.body.style.backgroundColor="green";
-    const submit = document.submit.addEventListener('click',()=>{
-        const Driver =document.getElementById('driver');
-      })
+//     document.body.style.backgroundColor="green";
+//     const submit = document.submit.addEventListener('click',()=>{
+//         const Driver =document.getElementById('driver');
+//       })
     
-      document.getElementById("submit").addEventListener("click",findResult)
+//       document.getElementById("submit").addEventListener("click",findResult)
     
-    function findResult(){
-      //alert(inputDriver);
-      document.body.style.backgroundColor = "green";
-      console.log(inputDriver);
-    }
+//     function findResult(){
+//       //alert(inputDriver);
+//       document.body.style.backgroundColor = "green";
+//       console.log(inputDriver);
+//     }
     
-    function getFormInfo(){
-      const formF1 =document.forms["formF1Input"];
-      const faveTeam = document.getElementById("Team");
-      const faveDriver = document.getElementById("Driver");
-      const faveSeason = document.getElementById("Season");
+//     function getFormInfo(){
+//       const formF1 =document.forms["formF1Input"];
+//       const faveTeam = document.getElementById("Team");
+//       const faveDriver = document.getElementById("Driver");
+//       const faveSeason = document.getElementById("Season");
 
-    }
+//     }
 
-    function callDriversAPI(){
-      fetch('http://ergast.com/api/f1/drivers')
-      .then(response => response.json())
-      .then(json => console.log(json))
-    }
+//     function callDriversAPI(){
+//       fetch('http://ergast.com/api/f1/drivers')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+//     }
 
-    function callCircuitsAPI(){
-      fetch('http://ergast.com/api/f1/circuits')
-      .then(response => response.json())
-      .then(json => console.log(json))
-    }
+//     function callCircuitsAPI(){
+//       fetch('http://ergast.com/api/f1/circuits')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+//     }
